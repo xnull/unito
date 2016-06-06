2bcd42d6dc483fdd93ab3f75c289c57b0cc5046e
Vyacheslav Petc <v.petc@netris.ru>
2016-06-06 13:57:19 +0300

    AbstractFixture added dependsOn method

--- src/main/java/unito/AbstractFixture.java
+++ src/main/java/unito/AbstractFixture.java
@@ -2,6 +2,8 @@ package unito;
 
 import unito.api.Fixture;
 
+import java.util.List;
+
 /**
  * Contains test data
  * Created by bynull on 03.06.16.
@@ -10,10 +12,13 @@ public abstract class AbstractFixture<T> implements Fixture<T> {
     protected T data;
 
     public AbstractFixture() {
+        dependsOn();
         setup();
     }
 
     public abstract void setup();
+
+    public abstract List<? extends AbstractFixture<?>> dependsOn();
 
     @Override
     public T getData() {
